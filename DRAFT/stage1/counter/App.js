20401fbdea76bd3bf8104baf70b89ab6ec801fe9
Je <git@iamje.com>
2020-12-04 01:53:33 +0800

    chore(draft): format

--- DRAFT/stage1/counter/App.js
+++ DRAFT/stage1/counter/App.js
@@ -1,92 +1,92 @@
 /* --- alef helper code --- */
 
 class Component {
-    el = null
-    disposes = []
-    nodes = []
-    
-    mount(el) {
-        this.el = el
-        this.nodes.forEach(node => append(el, node))
+  el = null
+  disposes = []
+  nodes = []
+  mount(el) {
+    this.el = el
+    this.nodes.forEach(node => append(el, node))
+  }
+  unmount() {
+    this.disposes.forEach(dispose => dispose())
+    if (this.el) {
+      this.nodes.forEach(node => remove(this.el, node))
     }
-    unmount() {
-        this.disposes.forEach(dispose => dispose())
-        if (this.el) {
-            this.nodes.forEach(node => remove(this.el, node))
-        }
-    }
+  }
 }
 
 function Element(name, parent) {
-    const el = document.createElement(name)
-    if (parent) {
-        append(parent, el)
-    }
-    return el
+  const el = document.createElement(name)
+  if (parent) {
+    append(parent, el)
+  }
+  return el
 }
 
 function Text(text, parent) {
-    const node = document.createTextNode(text)
-    if (parent) {
-        append(parent, node)
-    }
-    return node
+  const node = document.createTextNode(text)
+  if (parent) {
+    append(parent, node)
+  }
+  return node
 }
 
 function append(parent, child) {
-    parent.appendChild(child)
+  parent.appendChild(child)
 }
 
 function remove(parent, child) {
-    parent.removeChild(child)
+  parent.removeChild(child)
 }
 
 function setTextContent(node, text) {
-    node.textContent = String(text)
+  node.textContent = String(text)
 }
 
 function listen(el, evName, callback, update) {
-    const cb = () => {
-        callback()
-        update()
-    }
-    el.addEventListener(evName, cb)
-    return () => el.removeEventListener(evName, cb)
+  const cb = () => {
+    callback()
+    update()
+  }
+  el.addEventListener(evName, cb)
+  return () => el.removeEventListener(evName, cb)
 }
 
 /* --- END --- */
 
 export default class App extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super()
 
-        let n = 0
+    let n = 0
 
-        const handler = () => {
-            n++
-        }
+    // button[0].onClick
+    const handler = () => {
+      n++
+    }
+    // button[1].onClick
+    const handler2 = () => {
+      n--
+    }
 
-        const handler2 = () => {
-            n--
-        }
+    let p = Element('p')
+    let t = Text('current count is ', p)
+    let t2 = Text(n, p)
+    let button = Element('button')
+    let t3 = Text('+', button)
+    let t4 = Text('\n')
+    let button2 = Element('button')
+    let t5 = Text('-', button2)
 
-        let p = Element('p')
-        let t = Text('current count is ', p)
-        let t2 = Text(n, p)
-        let button = Element('button')
-        let t3 = Text('+', button)
-        let t4 = Text('\n')
-        let button2 = Element('button')
-        let t5 = Text('-', button2)
-
-        this.nodes = [p, button, t4, button2]
-        this.disposes = [
-            listen(button, 'click', handler, () => {
-                setTextContent(t2, n)
-            }),
-            listen(button2, 'click', handler2, () => {
-                setTextContent(t2, n)
-            })
-        ]
-    } 
+    this.nodes = [p, button, t4, button2]
+    this.disposes = [
+      listen(button, 'click', handler, () => {
+        setTextContent(t2, n)
+      }),
+      listen(button2, 'click', handler2, () => {
+        setTextContent(t2, n)
+      })
+    ]
+  }
 }
--- DRAFT/stage1/hello-world/App.js
+++ DRAFT/stage1/hello-world/App.js
@@ -1,57 +1,56 @@
 /* --- alef helper code --- */
 
 class Component {
-    el = null
-    disposes = []
-    nodes = []
-    
-    mount(el) {
-        this.el = el
-        this.nodes.forEach(node => append(el, node))
+  el = null
+  disposes = []
+  nodes = []
+  mount(el) {
+    this.el = el
+    this.nodes.forEach(node => append(el, node))
+  }
+  unmount() {
+    this.disposes.forEach(dispose => dispose())
+    if (this.el) {
+      this.nodes.forEach(node => remove(this.el, node))
     }
-    unmount() {
-        this.disposes.forEach(dispose => dispose())
-        if (this.el) {
-            this.nodes.forEach(node => remove(this.el, node))
-        }
-    }
+  }
 }
 
 function Element(name, parent) {
-    const el = document.createElement(name)
-    if (parent) {
-        append(parent, el)
-    }
-    return el
+  const el = document.createElement(name)
+  if (parent) {
+    append(parent, el)
+  }
+  return el
 }
 
 function Text(text, parent) {
-    const node = document.createTextNode(text)
-    if (parent) {
-        append(parent, node)
-    }
-    return node
+  const node = document.createTextNode(text)
+  if (parent) {
+    append(parent, node)
+  }
+  return node
 }
 
 function append(parent, child) {
-    parent.appendChild(child)
+  parent.appendChild(child)
 }
 
 function remove(parent, child) {
-    parent.removeChild(child)
+  parent.removeChild(child)
 }
 
 /* --- END --- */
 
-export default class App extends Component{
-    constructor() {
-        super()
+export default class App extends Component {
+  constructor() {
+    super()
 
-        let name = 'world'
+    let name = 'world'
 
-        const p = Element('p')
-        const t = Text(`hello ${name}!`, p)
-        
-        this.nodes = [p]
-    }
+    const p = Element('p')
+    const t = Text(`hello ${name}!`, p)
+
+    this.nodes = [p]
+  }
 }
