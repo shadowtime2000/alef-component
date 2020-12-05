90406c3b754d186a687f8e52e109b00770373f1e
X <i@jex.me>
2020-12-05 11:58:24 +0800

    Update App.alef.jsx

--- DRAFT/stage-1/counter/App.alef.jsx
+++ DRAFT/stage-1/counter/App.alef.jsx
@@ -1,8 +1,8 @@
 let n = 0
 
-$t: <p>current count is {n}</p>
-$t: <button onClick={() => { n-- }}>-</button>
-$t: <button onClick={() => { n++ }}>+</button>
+$template: <p>current count is {n}</p>
+$template: <button onClick={() => { n-- }}>-</button>
+$template: <button onClick={() => { n++ }}>+</button>
 
 $style: `
   p: {
