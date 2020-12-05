d190651d8b91ac0ec11a1b2f8c4020e4ff8b7230
X <i@jex.me>
2020-12-05 11:56:13 +0800

    Update App.alef.jsx

--- DRAFT/stage-1/counter/App.alef.jsx
+++ DRAFT/stage-1/counter/App.alef.jsx
@@ -3,7 +3,8 @@ let n = 0
 $t: <p>current count is {n}</p>
 $t: <button onClick={() => { n-- }}>-</button>
 $t: <button onClick={() => { n++ }}>+</button>
-$s: `
+
+$style: `
   p: {
     color: ${n > 10 : 'red' ? 'black'}
   }
