28cd34e1ae1726a605463029904c592906d597ae
X <i@jex.me>
2020-12-05 11:48:14 +0800

    Update App.alef.jsx

--- DRAFT/stage-1/fibonacci/App.alef.jsx
+++ DRAFT/stage-1/fibonacci/App.alef.jsx
@@ -6,5 +6,5 @@ function addNumber() {
 	numbers = [...numbers, (numbers[numbers.length - 2] || 0) + numbers[numbers.length - 1]]
 }
 
-<p>0 + {numbers.join(' + ')} = {sum}</p>;
-<button onClick={addNumber}>Add a number</button>;
+$t: <p>0 + {numbers.join(' + ')} = {sum}</p>
+$t: <button onClick={addNumber}>Add a number</button>
