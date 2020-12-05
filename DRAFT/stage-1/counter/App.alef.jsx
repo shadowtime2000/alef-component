09f9614a838ba1ff537c2437bf6ea9cd3611c9c5
X <i@jex.me>
2020-12-05 12:03:10 +0800

    remove $style

--- DRAFT/stage-1/counter/App.alef.jsx
+++ DRAFT/stage-1/counter/App.alef.jsx
@@ -3,9 +3,3 @@ let n = 0
 $t: <p>current count is {n}</p>
 $t: <button onClick={() => { n-- }}>-</button>
 $t: <button onClick={() => { n++ }}>+</button>
-
-$style: `
-  p: {
-    color: ${n > 10 : 'red' ? 'black'}
-  }
-`
