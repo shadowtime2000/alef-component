3d868ad27fe7d3981dd0887559609cab3c9ed505
ije <git@iamje.com>
2020-12-13 00:05:40 +0800

    chore(compiler): update build script

--- compiler/build.ts
+++ compiler/build.ts
@@ -1,1 +1,23 @@
 /* Build alef component comilper to WASM package */
+
+import * as base64 from 'https://deno.land/std@0.80.0/encoding/base64.ts'
+
+if (import.meta.main) {
+    const p = Deno.run({
+        cmd: ['wasm-pack', 'build', '--target', 'web'],
+        stdout: 'inherit',
+        stderr: 'inherit'
+    })
+    await p.status()
+    p.close()
+    const wasmData = await Deno.readFile('./pkg/alef_compiler_bg.wasm')
+    const data64 = base64.encode(wasmData)
+    await Deno.writeTextFile(
+        './wasm.js',
+        [
+            `const data = "${data64}";`,
+            `export default () => Uint8Array.from(atob(data), v => v.charCodeAt(0))`
+        ].join('\n')
+    )
+    await Deno.copyFile('./pkg/alef_compiler.js', './wasm-pack.js')
+}
