962ab3bef3aa88cd4870ba67972266bd2037e93c
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-12-18 12:27:57 -0800

    feat: parser created

--- compiler/src/ast/css.rs
+++ compiler/src/ast/css.rs
@@ -1,12 +1,22 @@
 // Copyright 2020 the The Alef Component authors. All rights reserved. MIT license.
 
 use swc_ecma_ast::*;
+use cssparser::*;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct CSS {}
 
 impl CSS {
   pub fn parse(tpl: &Tpl) -> Self {
-    CSS {}
+    // I should probably be looping over quasis values or something but I have no exact idea on what to do here
+    let quasis_first = tpl.quasis.first();
+    match quasis_first {
+      Some(p) => {
+        let input = &mut ParserInput::new(p.raw.value.as_ref());
+        let parser = &mut Parser::new(input);
+        CSS {}
+      },
+      None => CSS {}
+    }
   }
 }
