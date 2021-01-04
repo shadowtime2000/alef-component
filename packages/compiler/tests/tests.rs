85ae709bcf5d5effc2d74eadbd9f45cc92015dd8
Je <git@iamje.com>
2021-01-05 01:11:58 +0800

    chore(compiler): add `test_component_dirty` testing

--- packages/compiler/tests/tests.rs
+++ packages/compiler/tests/tests.rs
@@ -23,28 +23,44 @@ fn test_component_export() {
   assert!(code.contains("export default class App extends Component"));
   assert!(code.contains("constructor(props)"));
   assert!(code.contains("super(props)"));
-  assert!(code.contains("const nodes = ["));
   assert!(code.contains("this.register(nodes)"));
 }
 
 #[test]
 fn test_component_jsx() {
   let source = r#"
-    let n = 0
+    let name: string = 'World'
 
-    $t: <p>current count is <strong>{n}</strong></p>
-    $t: <button onClick={() => { n-- }}>-</button>
-    $t: <button onClick={() => { n++ }}>+</button>
+    $t: <p>Hello <strong>{name}</strong>!</p>
   "#;
   let (code, _) = t("App.alef", source);
-  assert!(code.contains("import { Component, Element, Memo, Dirty } from \"alef-dom\";"));
-  assert!(code.contains("Element(\"p\", null, \"current count is \", Element(\"strong\", null"));
+  assert!(code.contains("import { Component, Element, Memo } from \"alef-dom\";"));
+  assert!(code.contains("Element(\"p\", null, \"Hello \", Element(\"strong\", null"));
+
+  let r = Regex::new(r"Memo\(\(\)\s*=>\s*name\s*,\s*\[\s*0\s*\]\)").unwrap();
+  assert!(r.is_match(code.as_str()));
+}
+
+#[test]
+fn test_component_dirty() {
+  let source = r#"
+    let n = 0
+
+    function increase() {
+      n++
+    }
+
+    $t: <button onClick={e => { n-- }}>-</button>
+    $t: <button onClick={increase}>+</button>
+  "#;
+  let (code, _) = t("App.alef", source);
+  assert!(code.contains("import { Component, Element, Dirty } from \"alef-dom\";"));
   assert!(code.contains("Element(\"button\", {"));
 
-  let r = Regex::new(r"Memo\(\(\)\s*=>\s*n\s*,\s*\[\s*0\s*\]\)").unwrap();
-  assert!(r.is_match(code.as_str()));
   let r =
-    Regex::new(r"onClick:\s*Dirty\(\(\)\s*=>\s*\{\s*n(\+\+|--);?\s*\}\s*,\s*\[\s*0\s*\]\s*\)")
-      .unwrap();
+    Regex::new(r"onClick:\s*Dirty\(\(e\)\s*=>\s*\{\s*n--;?\s*\}\s*,\s*\[\s*0\s*\]\s*\)").unwrap();
+  assert!(r.is_match(code.as_str()));
+
+  let r = Regex::new(r"const increase = Dirty\(function increase\(\) \{\}, \[0\]\)").unwrap();
   assert!(r.is_match(code.as_str()));
 }
